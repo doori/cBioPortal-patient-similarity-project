f59d324725ced1485baedb69e0e8a97e64e399a5
drose <doori.rose@gmail.com>
2019-10-19 23:56:42 -0400

    Add h2 db, jpa, patient repository

--- cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/model/Patient.java
+++ cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/model/Patient.java
@@ -1,12 +1,36 @@
 package edu.gatech.cse6242.cbioportal.model;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Patient {
-
+    @Id
+    private Long id;
     private String patientId;
-    private int age;
     private String cancerType;
-    private List<String> alterations;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getCancerType() {
+        return cancerType;
+    }
+
+    public void setCancerType(String cancerType) {
+        this.cancerType = cancerType;
+    }
 }
--- cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/web/ApiController.java
+++ cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/web/ApiController.java
@@ -1,9 +1,10 @@
 package edu.gatech.cse6242.cbioportal.web;
 
+import edu.gatech.cse6242.cbioportal.model.Patient;
+import edu.gatech.cse6242.cbioportal.service.SimilarityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Arrays;
@@ -12,8 +13,14 @@ import java.util.Arrays;
 @RestController
 public class ApiController {
 
+    @Autowired
+    SimilarityService similarityService;
+
     @GetMapping("helloworld")
-    public ResponseEntity<List<String>> getPatients() {
-        return new ResponseEntity<>(Arrays.asList("hello world"), HttpStatus.OK);
+    public ResponseEntity<List<Patient>> getPatients() {
+        List<Patient> patients = similarityService.getPatientDetails(
+                Arrays.asList(0L, 1L, 2L));
+
+        return new ResponseEntity<>(patients, HttpStatus.OK);
     }
 }
