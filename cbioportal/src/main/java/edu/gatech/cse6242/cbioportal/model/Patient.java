079d3cbe174ed51661be8ef0a5a9f4e2ef949aad
drose <doori.rose@gmail.com>
2019-10-21 09:52:07 -0400

    H2 database, load patient, sample data

--- cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/model/Patient.java
+++ cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/model/Patient.java
@@ -1,14 +1,17 @@
 package edu.gatech.cse6242.cbioportal.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Patient {
     @Id
     private Long id;
     private String patientId;
-    private String cancerType;
+    private String sex;
+    private String vitalStatus;
+    private String smokingHistory;
+    private String osMonths;
+    private String osStatus;
 
     public Long getId() {
         return id;
@@ -26,11 +29,43 @@ public class Patient {
         this.patientId = patientId;
     }
 
-    public String getCancerType() {
-        return cancerType;
+    public String getSex() {
+        return sex;
     }
 
-    public void setCancerType(String cancerType) {
-        this.cancerType = cancerType;
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getVitalStatus() {
+        return vitalStatus;
+    }
+
+    public void setVitalStatus(String vitalStatus) {
+        this.vitalStatus = vitalStatus;
+    }
+
+    public String getSmokingHistory() {
+        return smokingHistory;
+    }
+
+    public void setSmokingHistory(String smokingHistory) {
+        this.smokingHistory = smokingHistory;
+    }
+
+    public String getOsMonths() {
+        return osMonths;
+    }
+
+    public void setOsMonths(String osMonths) {
+        this.osMonths = osMonths;
+    }
+
+    public String getOsStatus() {
+        return osStatus;
+    }
+
+    public void setOsStatus(String osStatus) {
+        this.osStatus = osStatus;
     }
 }
---
+++ cbioportal/src/main/java/edu/gatech/cse6242/cbioportal/model/Sample.java
@@ -0,0 +1,161 @@
+package edu.gatech.cse6242.cbioportal.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Sample {
+    @Id
+    private Long id;
+    private String patientId;
+    private String sampleId;
+    private String sampleCollectionSource;
+    private String specimenPreservationType;
+    private String specimenType;
+    private String dnaInput;
+    private String sampleCoverage;
+    private String tumorPurity;
+    private String matchedStatus;
+    private String sampleType;
+    private String primarySite;
+    private String metastaticSite;
+    private String sampleClass;
+    private String oncotreeCode;
+    private String cancerType;
+    private String cancerTypeDetailed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getSampleId() {
+        return sampleId;
+    }
+
+    public void setSampleId(String sampleId) {
+        this.sampleId = sampleId;
+    }
+
+    public String getSampleCollectionSource() {
+        return sampleCollectionSource;
+    }
+
+    public void setSampleCollectionSource(String sampleCollectionSource) {
+        this.sampleCollectionSource = sampleCollectionSource;
+    }
+
+    public String getSpecimenPreservationType() {
+        return specimenPreservationType;
+    }
+
+    public void setSpecimenPreservationType(String specimenPreservationType) {
+        this.specimenPreservationType = specimenPreservationType;
+    }
+
+    public String getSpecimenType() {
+        return specimenType;
+    }
+
+    public void setSpecimenType(String specimenType) {
+        this.specimenType = specimenType;
+    }
+
+    public String getDnaInput() {
+        return dnaInput;
+    }
+
+    public void setDnaInput(String dnaInput) {
+        this.dnaInput = dnaInput;
+    }
+
+    public String getSampleCoverage() {
+        return sampleCoverage;
+    }
+
+    public void setSampleCoverage(String sampleCoverage) {
+        this.sampleCoverage = sampleCoverage;
+    }
+
+    public String getTumorPurity() {
+        return tumorPurity;
+    }
+
+    public void setTumorPurity(String tumorPurity) {
+        this.tumorPurity = tumorPurity;
+    }
+
+    public String getMatchedStatus() {
+        return matchedStatus;
+    }
+
+    public void setMatchedStatus(String matchedStatus) {
+        this.matchedStatus = matchedStatus;
+    }
+
+    public String getSampleType() {
+        return sampleType;
+    }
+
+    public void setSampleType(String sampleType) {
+        this.sampleType = sampleType;
+    }
+
+    public String getPrimarySite() {
+        return primarySite;
+    }
+
+    public void setPrimarySite(String primarySite) {
+        this.primarySite = primarySite;
+    }
+
+    public String getMetastaticSite() {
+        return metastaticSite;
+    }
+
+    public void setMetastaticSite(String metastaticSite) {
+        this.metastaticSite = metastaticSite;
+    }
+
+    public String getSampleClass() {
+        return sampleClass;
+    }
+
+    public void setSampleClass(String sampleClass) {
+        this.sampleClass = sampleClass;
+    }
+
+    public String getOncotreeCode() {
+        return oncotreeCode;
+    }
+
+    public void setOncotreeCode(String oncotreeCode) {
+        this.oncotreeCode = oncotreeCode;
+    }
+
+    public String getCancerType() {
+        return cancerType;
+    }
+
+    public void setCancerType(String cancerType) {
+        this.cancerType = cancerType;
+    }
+
+    public String getCancerTypeDetailed() {
+        return cancerTypeDetailed;
+    }
+
+    public void setCancerTypeDetailed(String cancerTypeDetailed) {
+        this.cancerTypeDetailed = cancerTypeDetailed;
+    }
+}
