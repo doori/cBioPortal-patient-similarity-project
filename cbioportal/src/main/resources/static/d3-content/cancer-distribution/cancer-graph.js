1c4d50aa1eca7fc7238163c2115da477f6fea4fa
dfelipe3 <dfelipe3@gatech.edu>
2019-11-15 02:03:41 -0500

    Changes for cancer distribution graph

--- cbioportal/src/main/resources/static/d3-content/cancer-distribution/cancer-graph.js
+++ cbioportal/src/main/resources/static/d3-content/cancer-distribution/cancer-graph.js
@@ -5,119 +5,112 @@ async function getCancerDistributionGraph(patientId) {
 
     let result = await client.GetCancerTypePrediction(patientId);
 
-    patientCancerDistribution = result.data;
+    patientCancerDistribution = result.data.classDistribution;
 }
 
 
 
-function doDrawCancerDistributionGraph(data) {
+function doDrawCancerDistributionGraph(cancerDistributionRawData) {
+    const cancerTypeElemLimit = 10;
+
+    let cancerDistributionData = [];
+
+    Object.keys(cancerDistributionRawData).map((key) => {
+        var value = cancerDistributionRawData[key];
+        cancerDistributionData.push({ 'key': key, 'value': value });
+    });
+
+    const tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
     let parent = d3.select("#patient-similarity-cancer-distribution");
-    console.log('parent.node().getBoundingClientRect().width')
-    console.log(parent.node().getBoundingClientRect().width)
 
-    console.log('parent.node().getBoundingClientRect().height')
-    console.log(parent.node().getBoundingClientRect().height) //350
+    let sortedCancerDistribution = cancerDistributionData.sort((a, b) => b.value - a.value);
+    let filteredCancerDistribution = sortedCancerDistribution.slice(0, cancerTypeElemLimit);
 
-    // let svg = parent.append("svg")
+    let margin = {
+        top: 20,
+        right: 50,
+        bottom: 150,
+        left: 40,
+    },
+        width = parent.node().getBoundingClientRect().width - 50,
+        height = parent.node().getBoundingClientRect().height - margin.bottom;
 
-    // console.log('+parent.style("width").slice(0, -2) + 200');
-    // console.log(+parent.style("width").slice(0, -2) + 200)
+    let svg = parent.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
 
-    // // let svg = d3.select("#patient-similarity-cancer-distribution"),
-    // let margin = {
-    //     top: 20,
-    //     right: 20,
-    //     bottom: 20,
-    //     left: 50,
-    // };
-    // // width = +svg.attr("width") - margin.left - margin.right,
-    // // height = +svg.attr("height") - margin.top - margin.bottom;
-    // const tooltip = d3.select("body").append("div").attr("class", "toolTip");
-    // let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    //     y = d3.scaleLinear().rangeRound([height, 0]);
-    // let g = svg.append("g")
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    // //import test data from csv and roll up to get the frequency for each type of cancer
-    // //result is array of objects
+    let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // // let cancerCount = d3.nest()
-    // //     .key(function (d) { return d.CANCER_TYPE; })
-    // //     .rollup(function (v) { return v.length; })
-    // //     .entries(data);
+    var x = d3.scaleBand()
+        .rangeRound([0, width])
+        .padding(0.1);
 
-    // const cancerCount = [
-    //     { key: "Some Cancer Type A", value: "123" },
-    //     { key: "Some Cancer Type B", value: "456" },
-    //     { key: "Some Cancer Type C", value: "789" },
-    // ]
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
 
+    x.domain(filteredCancerDistribution.map(function (d) {
+        return d.key;
+    }));
 
+    y.domain([0, d3.max(filteredCancerDistribution, function (d) {
+        return Number(d.value);
+    })]);
 
-    // //sort array by count, descending + limit to the top 15
-    // cancerCount.sort((a, b) => (a.value > b.value) ? -1 : 1)
-    // let top15 = cancerCount.slice(0, 15);
-    // console.log('JSON.stringify(top15)');
-    // console.log(JSON.stringify(top15));
-    // //basic sum function
-    // const mysum = function (items, prop) {
-    //     return items.reduce(function (a, b) {
-    //         return a + b[prop];
-    //     }, 0);
-    // };
-    // //get the overall # of patients
-    // mytotal = mysum(cancerCount, 'value');
-    // console.log(mytotal)
-    // //group cancer types beyond the top 15 into an aggregated other
-    // othertotal = mytotal - mysum(top15, 'value');
-    // console.log(othertotal)
-    // other_obj = { "key": "Other Cancer Types", "value": othertotal }
-    // console.log(other_obj)
-    // //add other object to the top 15 array of objects
-    // top15.push(other_obj)
-    // //set the x axis and y axis based on top 15 array then actually adding the x axis
-    // x.domain(top15.map(function (d) { return d.key }))
-    // y.domain([0, d3.max(top15, function (d) { return (d.value / mytotal) })]);
-    // g.append("g")
-    //     .attr("class", "axis axis--x")
-    //     .attr("transform", "translate(0," + height + ")")
-    //     .call(d3.axisBottom(x));
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("y", 10)
+        .call(wrap, x.bandwidth());
 
-    // g.selectAll("text")
-    //     .attr("transform", "rotate(90)")
-    //     .attr("x", 10)
-    //     .attr("y", 0)
-    //     .attr("dy", ".35em")
-    //     .style("text-anchor", "start");
-    // //formatting and setting up of Y axis and adding title
-    // let formatPercent = d3.format(".0%");
+    g.append("g")
+        .call(d3.axisLeft(y))
 
-    // g.append("g")
-    //     .attr("class", "axis axis--y")
-    //     .call(d3.axisLeft(y).ticks(10))
+    g.selectAll(".bar")
+        .data(filteredCancerDistribution)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return x(d.key);
+        })
+        .attr("y", function (d) {
+            return y(Number(d.value));
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) {
+            return height - y(Number(d.value));
+        })
+        .on("mousemove", function (d) {
+            tooltip
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY - 70 + "px")
+                .style("display", "inline-block")
+                .html((d.key) + "<br>" + ((d.value / 100) * 100).toFixed(2) + "%" + " | " + (d.value));
+        })
+        .on("mouseout", function (d) { tooltip.style("display", "none"); });
 
-    // g.append("text")
-    //     .attr('x', 100)
-    //     .attr('y', -1)
-    //     .attr("font-family", "sans-serif")
-    //     .attr("font-size", "16px")
-    //     .attr("font-weight", "bold")
-    //     .text('Distribution of Cancer Types: Top 15 Types + Aggregated Other');
-
-    // //adding the bar chart and mouseover
-    // g.selectAll(".bar")
-    //     .data(top15)
-    //     .enter().append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", function (d) { return x(d.key); })
-    //     .attr("y", function (d) { return y((d.value) / mytotal); })
-    //     .attr("width", x.bandwidth())
-    //     .attr("height", function (d) { return height - y(d.value / mytotal); })
-    //     .on("mousemove", function (d) {
-    //         tooltip
-    //             .style("left", d3.event.pageX - 50 + "px")
-    //             .style("top", d3.event.pageY - 70 + "px")
-    //             .style("display", "inline-block")
-    //             .html((d.key) + "<br>" + ((d.value / mytotal) * 100).toFixed(2) + "%" + " | " + (d.value));
-    //     })
-    //     .on("mouseout", function (d) { tooltip.style("display", "none"); });
+    function wrap(text, width) {
+        text.each(function () {
+            let text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    }
 }
